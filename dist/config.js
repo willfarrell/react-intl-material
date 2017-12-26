1669e6c46714f7a7cf23272363c2de13feffdc97
will Farrell <will.farrell@gmail.com>
2017-12-25 17:13:56 -0700

    Clean up

--- dist/config.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-var countryLanguages = exports.countryLanguages = ['en', 'ar', 'de', 'es', 'fr', 'it', 'ru', 'zh'];
-var regionLanguages = exports.regionLanguages = ['en', 'ar', 'de', 'es', 'fr', 'it', 'ru', 'zh'];
