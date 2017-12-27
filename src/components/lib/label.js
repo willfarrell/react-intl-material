98beeee549a5ea66d9cc9fd245299dd925fd7ffa
will Farrell <will.farrell@gmail.com>
2017-12-26 19:37:50 -0700

    refactor fixes

--- src/components/lib/label.js
+++ src/components/lib/label.js
@@ -9,19 +9,19 @@ export const makeHasError = (props) => {
     return props.touched && !!(props.error || props.errorIntl);
 };
 
-export const makeLabel = (props) => {
+export const makeLabel = (props, values = {}) => {
     return props.labelIntl
-        || makeStr('label', props.intl, props.name, props.label);
+        || makeStr('label', props.intl, props.name, props.label, values);
 };
 
 
-export const makePlaceholder = (props) => {
+export const makePlaceholder = (props, values = {}) => {
     return props.placeholderIntl
-        || makeStr('placeholder', props.intl, props.name, props.placeholder);
+        || makeStr('placeholder', props.intl, props.name, props.placeholder, values);
 };
 
 
-export const makeHelperText = (props, values) => {
+export const makeHelperText = (props, values = {}) => {
     const hasError = makeHasError(props);
     return (hasError && props.error && props.intl.formatMessage({id: props.error}, values)) // override with error
         || hasError && props.errorIntl
